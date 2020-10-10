0d68979e85fa4123b801e12678b6599094a52fde
isonlaxman <32971829+isonlaxman@users.noreply.github.com>
2020-10-10 23:29:39 +0530

    country data from trends

--- scripts/trends/by_country.py
+++ scripts/trends/by_country.py
@@ -1,34 +1,25 @@
+## %
 from pytrends.request import TrendReq
 from addon import *
 import pandas as pd
 import time
 import pycountry
 
-text_file = open("/data/trends/countries.txt", "r")
+## %
+text_file = open("./data/trends/countries.txt", "r")
 lines = text_file.read().split(',')
-
-countries = {}
-for country in pycountry.countries:
-    countries[country.name] = country.alpha_2
 
 print(len(lines))
 
 pytrends = TrendReq(hl='en-US', tz=360)
 
+## %
 kw_list = ["covid symptoms"]
 df = pd.DataFrame()
 
-for c in lines:
-    try:
-        pytrends.build_payload(kw_list, cat=0, timeframe='2020-03-22 2020-10-09', geo=countries[c], gprop='')
-        country_df = pytrends.interest_over_time()
-        country_df["Country_Name"] = c
-        country_df["Country_Code"] = countries[c]
-        df = pd.concat([df, country_df])
-        time.sleep(.1)
-    # print(pytrends.interest_by_region(resolution="COUNTRY"))
-    except:
-        print("fail", c)
-        break
-
-df.to_csv("/data/trend/countries_trend.csv")
+pytrends.build_payload(kw_list, cat=0, timeframe='2020-03-22 2020-10-09', geo="US", gprop='')
+country_df = pytrends.interest_over_time()
+country_df["Country_Name"] = "US"
+country_df["Country_Code"] = "US"
+df = pd.concat([df, country_df])
+df.to_csv("./data/trends/countries_trend_USA.csv", index=False)
