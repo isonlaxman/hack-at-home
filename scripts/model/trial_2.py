0d80834039cc4575da6f35e28cce839cc288afd0
isonlaxman <32971829+isonlaxman@users.noreply.github.com>
2020-10-11 19:40:38 +0530

    trial 2 for andrew

---
+++ scripts/model/trial_2.py
@@ -0,0 +1,123 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+# %%
+df = pd.read_csv("../data/final/final_final.csv")
+
+
+# %%
+df = df[df["Province_State"] == "New York"]
+df = df.drop(["Province_State", "Lat", "Long_", "Confirmed", "Deaths", "STATE", "COUNTY", "POPESTIMATE2019", "Confirmed_Percent", "Deaths_Percent", "Confirmed_Percent_Smooth", "Deaths_Percent_Smooth", "Deaths_Smooth"], axis=1)
+df["date"] = pd.to_datetime(df["date"])
+df
+
+
+# %%
+data = df.to_numpy()
+data
+
+
+# %%
+column_indices = {name: i for i, name in enumerate(df.columns)}
+
+# Split data
+n = len(df)
+train_df = df[0:int(n*0.7)]
+val_df = df[int(n*0.7):int(n*0.9)]
+test_df = df[int(n*0.9):]
+
+
+# %%
+df.dtypes
+
+
+# %%
+num_features = df.shape[1]
+
+df = df.drop(['date'], axis=1)
+
+train_mean = train_df.mean()
+train_std = train_df.std()
+
+# Normalization
+train_df = (train_df - train_mean) / train_std
+val_df = (val_df - train_mean) / train_std
+test_df = (test_df - train_mean) / train_std
+
+df_std = (df - train_mean) / train_std
+df_std = df_std.melt(var_name='Column', value_name='Normalized')
+plt.figure(figsize=(12, 6))
+ax = sns.violinplot(x='Column', y='Normalized', data=df_std)
+_ = ax.set_xticklabels(df.keys(), rotation=90)
+
+
+# %%
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+sample, sample_label = x_train[0], y_train[0]
+
+batch_size = 64
+# Each MNIST image batch is a tensor of shape (batch_size, 28, 28).
+# Each input sequence will be of size (28, 28) (height is treated like time).
+input_dim = 28
+
+units = 64
+output_size = 10  # labels are from 0 to 9
+
+# Build the RNN model
+def build_model(allow_cudnn_kernel=True):
+    # CuDNN is only available at the layer level, and not at the cell level.
+    # This means `LSTM(units)` will use the CuDNN kernel,
+    # while RNN(LSTMCell(units)) will run on non-CuDNN kernel.
+    if allow_cudnn_kernel:
+        # The LSTM layer with default options uses CuDNN.
+        lstm_layer = keras.layers.LSTM(units, input_shape=(None, input_dim))
+    else:
+        # Wrapping a LSTMCell in a RNN layer will not use CuDNN.
+        lstm_layer = keras.layers.RNN(
+            keras.layers.LSTMCell(units), input_shape=(None, input_dim)
+        )
+    model = keras.models.Sequential(
+        [
+            lstm_layer,
+            keras.layers.BatchNormalization(),
+            keras.layers.Dense(output_size),
+        ]
+    )
+    return model
+
+
+model = build_model(allow_cudnn_kernel=True)
+
+model.compile(
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    optimizer="sgd",
+    metrics=["accuracy"],
+)
+
+
+model.fit(
+    x_train, y_train, validation_data=(x_test, y_test), batch_size=batch_size, epochs=1
+)
+
+noncudnn_model = build_model(allow_cudnn_kernel=False)
+noncudnn_model.set_weights(model.get_weights())
+noncudnn_model.compile(
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    optimizer="sgd",
+    metrics=["accuracy"],
+)
+noncudnn_model.fit(
+    x_train, y_train, validation_data=(x_test, y_test), batch_size=batch_size, epochs=1
+)
+
+
